c812a72af1069cf2b36b099c4ba802ae46270451
Nathaniel Titterton <nate@berkeley.edu>
2012-04-02 16:17:05 -0700

    Edited isa.dtd and updated randomNumber.isa and Random.java to match.

--- examples/Random/default/Random.java
+++ examples/Random/default/Random.java
@@ -4,7 +4,7 @@ public class Random {
 	public int randomOneToTen(){		
 		//Assume we have a method getRandom() which returns a double with range [0,1).
 		//Fill in the textbox below to change that into a random integer with range [1,10].
-		return getRandom() * ;
+		return getRandom() *        ;
 		
 	}
 	
