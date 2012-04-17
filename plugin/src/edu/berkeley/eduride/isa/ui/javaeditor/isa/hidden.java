1e9d09e69483fa04f07ef80ad77accfd9e9eaabe
Luke Lu <luke_lmn@berkeley.edu>
2012-04-17 15:36:30 -0700

    committed isaParser

---
+++ plugin/src/edu/berkeley/eduride/isa/ui/javaeditor/isa/hidden.java
@@ -0,0 +1,39 @@
+package edu.berkeley.eduride.isa.ui.javaeditor.isa;
+public class hidden {
+	private String name;
+	private String tag;
+	
+	public hidden (String name, String tag){
+		setName(name);
+		setTag(tag);
+	}
+
+	/**
+	 * @return this.name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * @return this.tag
+	 */
+	public String getTag() {
+		return tag;
+	}
+
+	/**
+	 * @param name set to this.name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @param set to this.tag
+	 */
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+}
